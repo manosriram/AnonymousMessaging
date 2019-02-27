d699b9ec2f74584487ba5bf671562ab5fffe6999
Mano Sriram <mano.sriram0@gmail.com>
2019-02-27 21:16:43 +0530

    Moved to MongoDB Atlas.

--- setup/url.js
+++ setup/url.js
@@ -1,4 +1,5 @@
 module.exports = {
-  mongoURL: "mongodb://manosriram:mano1234@ds225375.mlab.com:25375/anonyou",
+  mongoURL:
+    "mongodb://manoSr:mano1234@cluster0-shard-00-00-ulrpt.mongodb.net:27017,cluster0-shard-00-01-ulrpt.mongodb.net:27017,cluster0-shard-00-02-ulrpt.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true",
   secret: "mano1234"
 };
